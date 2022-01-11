66f90b19d13975824df3a21650d09c92118f0abe
Panindra Chekoori <Panindra.Chekoori@grainger.com>
2022-01-11 08:51:42 -0600

    Update java runtime to 11 and add default construct to product event

--- common-code/src/main/java/product/pipeline/common/ProductEvent.java
+++ common-code/src/main/java/product/pipeline/common/ProductEvent.java
@@ -5,11 +5,14 @@ import java.util.StringJoiner;
 public class ProductEvent {
     public String sku;
     public String name;
-    public int quantity;
+    public Integer quantity;
     public String brand;
-    public int clearance;
+    public Integer clearance;
 
-    public ProductEvent(String sku, String name, int quantity, String brand, int clearance) {
+    public ProductEvent() {
+    }
+
+    public ProductEvent(String sku, String name, Integer quantity, String brand, Integer clearance) {
         this.sku = sku;
         this.name = name;
         this.quantity = quantity;
@@ -17,46 +20,7 @@ public class ProductEvent {
         this.clearance = clearance;
     }
 
-    public String getSku() {
-        return sku;
-    }
-
-    public void setSku(String sku) {
-        this.sku = sku;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public String getBrand() {
-        return brand;
-    }
-
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-
-    public int getClearance() {
-        return clearance;
-    }
-
-    public void setClearance(int clearance) {
-        this.clearance = clearance;
-    }
-
+    @Override
     public String toString() {
         return new StringJoiner(", ", ProductEvent.class.getSimpleName() + "[", "]")
                 .add("sku='" + sku + "'")
